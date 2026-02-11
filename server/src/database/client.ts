c8f973fae3c2bdc5831a61c8d350e4adc1fa7d29
chr1sdoofer <59119064+chr1sdoofer@users.noreply.github.com>
2026-02-11 23:48:44 +0100

    fix: update import path for PrismaClient in client.ts

--- server/src/database/client.ts
+++ server/src/database/client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../prisma/generated/prisma/client.js';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { env } from '../config/env.js';
 
