c6b53e7fc5f4a5dad3bc36ffe32abee931729a7e
humanbydefinition <christopherdietrich@outlook.com>
2026-02-13 11:31:15 +0100

    refactor: added `envPrefix` to vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,40 +6,37 @@ import tailwindcss from '@tailwindcss/vite';
 import { fallbackProxy } from './build/plugins/fallback-proxy';
 
 export default defineConfig({
-  envDir: path.resolve(__dirname, '..'),
-  plugins: [
-    react(),
-    tailwindcss(),
-    fallbackProxy({ target: 'http://localhost:3000' }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    cors: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        xfwd: true,
-      },
-      '/storage': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        xfwd: true,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
+	envDir: path.resolve(__dirname, '..'),
+	envPrefix: ['VITE_', 'PUBLIC_'],
+	plugins: [react(), tailwindcss(), fallbackProxy({ target: 'http://localhost:3000' })],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	server: {
+		cors: true,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+		},
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				changeOrigin: true,
+				xfwd: true,
+			},
+			'/storage': {
+				target: 'http://localhost:3000',
+				changeOrigin: true,
+				xfwd: true,
+			},
+		},
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+			},
+		},
+	},
 });
