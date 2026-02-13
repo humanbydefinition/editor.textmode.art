42c1c9c5b6641bf246b820ba33cf10218d8b16c2
humanbydefinition <christopherdietrich@outlook.com>
2026-02-13 17:53:19 +0100

    refactor: remove TextmodeManager and related type definitions

--- packages/client/src/engines/textmode/runtime/lib/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * textmode module - Textmode.js runtime components
- */
-
-export { TextmodeManager } from './TextmodeManager';
-export type { ITextmodeManager, SynthLayer } from './textmode.types';
