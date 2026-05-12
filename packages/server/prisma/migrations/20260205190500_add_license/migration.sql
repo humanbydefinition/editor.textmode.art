71f2d21cebf97bd4cc25c06d9f7ec65fc2647499
humanbydefinition <hello@textmode.art>
2026-05-13 00:53:52 +0200

    chore: removed backend related files

--- packages/server/prisma/migrations/20260205190500_add_license/migration.sql
+++
@@ -1,2 +0,0 @@
--- Add license field to SketchRequest
-ALTER TABLE "SketchRequest" ADD COLUMN "license" TEXT;
--- packages/server/prisma/migrations/20260319010000_remove_strudel_code/migration.sql
+++
@@ -1,3 +0,0 @@
--- Remove the deprecated Strudel code column from sketch submissions
-ALTER TABLE "SketchRequest"
-DROP COLUMN "strudelCode";
